b7625e4660e947917c8c38133b3679d821b6c6d1
Kaitlynnnnn <46434737+Kaitlynnnnn@users.noreply.github.com>
2019-01-11 23:29:51 +0800

    Update scripts.js

--- assets/js/src/scripts.js
+++ assets/js/src/scripts.js
@@ -32,33 +32,4 @@ $(document).ready(function() {
     mainClass: 'mfp-fade'
   });
 });
-     <script type="text/javascript">
-    /* 鼠标点击特效 */
-    var a_idx = 0;
-    jQuery(document).ready(function($) {
-        $("body").click(function(e) {
-    var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善");
-    var $i = $("<span/>").text(a[a_idx]);
-            a_idx = (a_idx + 1) % a.length;
-    var x = e.pageX,
-            y = e.pageY;
-            $i.css({
-    "z-index": 999999999999999999999999999999999999999999999999999999999999999999999,
-    "top": y - 20,
-    "left": x,
-    "position": "absolute",
-    "font-weight": "bold",
-    "color": "#ff6651"
-            });
-            $("body").append($i);
-            $i.animate({
-    "top": y - 180,
-    "opacity": 0
-            },
-            1500,
-    function() {
-                $i.remove();
-            });
-        });
-    });
-    </script>
+  
