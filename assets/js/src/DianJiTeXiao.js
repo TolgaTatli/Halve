57e66a90467e2856e471cdb3830142d3004ed1d5
Kaitlynnnnn <46434737+Kaitlynnnnn@users.noreply.github.com>
2019-01-12 00:19:01 +0800

    Update DianJiTeXiao.js

--- assets/js/src/DianJiTeXiao.js
+++ assets/js/src/DianJiTeXiao.js
@@ -2,7 +2,7 @@
 var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
-        var a = new Array("❤富强❤","❤民主❤","❤文明❤","❤和谐❤","❤自由❤","❤平等❤","❤公正❤","❤法治❤","❤爱国❤","❤敬业❤","❤诚信❤","❤友善❤","❤爱你哦❤");
+        var a = new Array("富强","民主","文明","和谐","自由","平等","公正","法治","爱国","敬业","诚信","友善","爱你哦","xD");
         var $i = $("<span></span>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
