c5586c8928f53841f872452ec7e9159a39d29790
Kaitlynnnnn <46434737+Kaitlynnnnn@users.noreply.github.com>
2019-01-13 14:19:57 +0800

    Update autoload.js

--- assets/autoload.js
+++ assets/autoload.js
@@ -1,8 +1,8 @@
 try {
-    $("<link>").attr({href: "assets/waifu.min.css?v=1.4.2", rel: "stylesheet", type: "text/css"}).appendTo('head');
+    $("<link>").attr({href: "Kaitlynnnnn.github.io/assets/waifu.min.css?v=1.4.2", rel: "stylesheet", type: "text/css"}).appendTo('head');
     $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
-    $.ajax({url: 'assets/waifu-tips.min.js?v=1.4.2',dataType:"script", cache: true, async: false});
-    $.ajax({url: 'assets/live2d.min.js?v=1.0.5',dataType:"script", cache: true, async: false});
+    $.ajax({url: 'Kaitlynnnnn.github.io/assets/waifu-tips.min.js?v=1.4.2',dataType:"script", cache: true, async: false});
+    $.ajax({url: 'Kaitlynnnnn.github.io/assets/live2d.min.js?v=1.0.5',dataType:"script", cache: true, async: false});
     /* 可直接修改部分参数 */
     live2d_settings['hitokotoAPI'] = 'hitokoto.cn';  // 一言 API
     live2d_settings['modelId'] = 5;                  // 默认模型 ID
